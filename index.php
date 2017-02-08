4dadc45d90ae2db89aa2520805d6109b9e6542f8
susviela <power.informatica.liv@gmail.com>
2017-02-08 15:35:54 -0200

    novas atualizações

---
+++ index.php
@@ -0,0 +1,10 @@
+<!--
+            - - - - - - - - - - - - - - - - - - - - - - - -
+            Sistema:    POWER Bel.
+            Função:     Gestão comercial de salões de beleza
+            Autor:      Carlos Alberto Nunes Susviela
+            Contato:    http://www.powerinformatica.com.br
+            Giithub:    https://github.com/susviela/power-bel
+            - - - - - - - - - - - - - - - - - - - - - - - -
+-->
+<?php header("location: site/index.php"); ?>
---
+++ site/index.php
@@ -0,0 +1,9 @@
+<!--
+            - - - - - - - - - - - - - - - - - - - - - - - -
+            Sistema:    POWER Bel.
+            Função:     Gestão comercial de salões de beleza
+            Autor:      Carlos Alberto Nunes Susviela
+            Contato:    http://www.powerinformatica.com.br
+            Giithub:    https://github.com/susviela/power-bel
+            - - - - - - - - - - - - - - - - - - - - - - - -
+-->
